73720f97711f21f74320786476ee1fa21ae8edf3
Raghav Nayak <ragsnayak@gmail.com>
2024-09-19 23:28:16 +0530

    added custom max heap implementation

--- src/heap/custom_heap_implementation.py
+++ src/heap/custom_heap_implementation.py
@@ -1,5 +1,3 @@
-
-
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -58,6 +56,77 @@ class MinHeap:
         return root
 
     def peek(self):
+        if len(self.heap) == 0:
+            return None
+        return self.heap[0]
+
+    def __str__(self):
+        return str(self.heap)
+
+
+#############
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, index):
+        return (index - 1) // 2
+
+    def left_child(self, index):
+        return 2 * index + 1
+
+    def right_child(self, index):
+        return 2 * index + 2
+
+    # Ensures that the heap property is maintained
+    # Parent node is larger than or equal to its children
+    def heapify_down(self, index):
+        largest = index
+        left = self.left_child(index)
+        right = self.right_child(index)
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != index:
+            # Swap the index and the largest element's index
+            self.heap[index], self.heap[largest] = (
+                self.heap[largest],
+                self.heap[index],
+            )
+            self.heapify_down(largest)
+
+    def heapify_up(self, index):
+        # While the node's parent is smaller than the node itself
+        while index > 0 and self.heap[self.parent(index)] < self.heap[index]:
+            self.heap[index], self.heap[self.parent(index)] = (
+                self.heap[self.parent(index)],
+                self.heap[index],
+            )
+            index = self.parent(index)
+
+    def insert(self, key):
+        # Insert the new key at the end and move it up to restore heap property
+        self.heap.append(key)
+        self.heapify_up(len(self.heap) - 1)
+
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        # Replace the root with the last element and restore heap property
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+        return root
+
+    def peek(self):
+        # Return the maximum element (the root of the heap)
         if len(self.heap) == 0:
             return None
         return self.heap[0]
