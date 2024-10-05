13967a02723a97c15a03e92bff3c1564066e9759
Raghav Nayak <ragsnayak@gmail.com>
2024-10-05 16:43:54 +0530

    solution for reversed string using inbuilt functions

---
+++ src/strings/reverse_words.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
+
+# 151. Reverse Words in a String
+# Given an input string s, reverse the order of the words.
+
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+# Return a string of the words in reverse order concatenated by a single space.
+
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+# Example 1:
+# Input: s = "the sky is blue"
+# Output: "blue is sky the"
+
+# Example 2:
+# Input: s = "  hello world  "
+# Output: "world hello"
+# Explanation: Your reversed string should not contain leading or trailing spaces.
+
+# Example 3:
+# Input: s = "a good   example"
+# Output: "example good a"
+# Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+
+# Constraints:
+# 1 <= s.length <= 104
+# s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+# There is at least one word in s.
+
+# Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+
+import re
+
+def reverse_words_using_inbuilt_func(s: str) -> str:
+    # return " ".join(s.split(" ")[-1::-1])
+    return re.sub(r"\s+", " ", " ".join(s.split(" ")[-1::-1])).strip()
+
+
+# def reverse_word(s: str) -> str:
+#     end = len(s) - 1
+#     reversed_str = str()
+
+#     while end >= 0:
+#         if s[end] == " ":
+#             end -= 1
+#         else:
+
+
+if __name__ == "__main__":
+    s = "the sky is blue"
+    print(f"{reverse_words_using_inbuilt_func(s)}")
+
+    s = "  hello world  "
+    print(f"{reverse_words_using_inbuilt_func(s)}")
+    
+
+    s = "a good   example"
+    print(f"{reverse_words_using_inbuilt_func(s)}")
