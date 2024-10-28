c96d9d27bda0bd935c4e1a0d6a8e43ef9244c5da
Raghav Nayak <ragsnayak@gmail.com>
2024-10-28 22:42:17 +0530

    solution for two sum using two pointers

--- src/arrays/two_sum_2.py
+++ src/arrays/two_sum_2.py
@@ -12,26 +12,59 @@
 
 
 # Example 1:
-
 # Input: numbers = [2,7,11,15], target = 9
 # Output: [1,2]
 # Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
-# Example 2:
 
+# Example 2:
 # Input: numbers = [2,3,4], target = 6
 # Output: [1,3]
 # Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
-# Example 3:
 
+# Example 3:
 # Input: numbers = [-1,0], target = -1
 # Output: [1,2]
 # Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
 
 
 # Constraints:
-
 # 2 <= numbers.length <= 3 * 104
 # -1000 <= numbers[i] <= 1000
 # numbers is sorted in non-decreasing order.
 # -1000 <= target <= 1000
 # The tests are generated such that there is exactly one solution.
+
+from typing import List
+
+# using two pointers
+def two_sum(numbers: List[int], target: int) -> List[int]:
+    if len(numbers) < 2:
+        return [-1, -1]
+
+    start = 0
+    end = len(numbers) - 1
+
+    while start < end:
+        sum = numbers[start] + numbers[end]
+        if sum == target:
+            return [start+1, end+1]
+        elif sum < target:
+            start += 1
+        else:
+            end -= 1
+
+    return [-1, -1]
+
+
+if __name__ == "__main__":
+    numbers = [2, 7, 11, 15]
+    target = 9
+    print(two_sum(numbers, target))
+    
+    numbers = [2,3,4]
+    target = 6
+    print(two_sum(numbers, target))
+    
+    numbers = [-1,0]
+    target = -1
+    print(two_sum(numbers, target))
