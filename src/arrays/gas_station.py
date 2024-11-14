5d2e8bc31bc3ac979493fb189d8cea1fca68eadd
Raghav Nayak <ragsnayak@gmail.com>
2024-11-14 22:39:16 +0530

    solution for gas station

--- src/arrays/gas_station.py
+++ src/arrays/gas_station.py
@@ -38,5 +38,31 @@
 from typing import List
 
 
-def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-    pass
+def can_complete_circuit_chatgpt(gas: List[int], cost: List[int]) -> int:
+    total_gas = sum(gas)
+    total_cost = sum(cost)
+
+    if total_gas < total_cost:
+        return -1
+
+    start = 0
+    balance = 0
+
+    for i in range(len(gas)):
+        balance += gas[i] - cost[i]
+
+        if balance < 0:
+            start = i + 1
+            balance = 0
+
+    return start
+
+
+if __name__ == "__main__":
+    gas = [1, 2, 3, 4, 5]
+    cost = [3, 4, 5, 1, 2]
+    print(can_complete_circuit_chatgpt(gas, cost))
+
+    gas = [2, 3, 4]
+    cost = [3, 4, 3]
+    print(can_complete_circuit_chatgpt(gas, cost))
