5b099a8469a8bc6257357b20e10c75272ba27e04
Raghav Nayak <ragsnayak@gmail.com>
2024-11-16 00:15:18 +0530

    best soln from leetcode for rotate matrix

--- src/generic/rotate_image.py
+++ src/generic/rotate_image.py
@@ -42,10 +42,37 @@ def rotate(matrix: List[List[int]]) -> None:
 
     return matrix
 
+# not sure this is the right one
+def rotate_best_soln_leetcode(matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    l, r = 0, len(matrix[0]) - 1
+
+    while l < r:
+        for i in range(r - l):
+            top = l
+            bottom = r
+
+            topLeft = matrix[top][l + i]
+
+            matrix[top][l + i] = matrix[bottom - i][l]
+
+            matrix[bottom - i][l] = matrix[bottom][r - i]
+
+            matrix[bottom][r - i] = matrix[top + i][r]
+
+            matrix[top + i][r] = topLeft
+        r -= 1
+        l += 1
+    return matrix
+
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print(rotate(matrix))
+    print(rotate_best_soln_leetcode(matrix))
 
     matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
     print(rotate(matrix))
+    print(rotate_best_soln_leetcode(matrix))
