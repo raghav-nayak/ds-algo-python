adfef69e9c1543c33dc81de3c1f78dfc879fc665
Raghav Nayak <ragsnayak@gmail.com>
2024-11-20 22:01:51 +0530

    class structure for insert delete getrandom

--- src/arrays/insert_delete_getrandom.py
+++ src/arrays/insert_delete_getrandom.py
@@ -32,3 +32,28 @@
 # -231 <= val <= 231 - 1
 # At most 2 * 105 calls will be made to insert, remove, and getRandom.
 # There will be at least one element in the data structure when getRandom is called.
+
+class RandomizedSet:
+
+    def __init__(self):
+        pass
+        
+
+    def insert(self, val: int) -> bool:
+        pass
+        
+
+    def remove(self, val: int) -> bool:
+        pass
+        
+
+    def getRandom(self) -> int:
+        pass
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
