a284025b48194d94b0dc8bcdd6bcc05c08281167
Raghav Nayak <ragsnayak@gmail.com>
2024-12-21 22:43:30 +0530

    problem description binary tree paths

---
+++ src/types/trees/binary_tree_paths.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/binary-tree-paths/description/?envType=problem-list-v2&envId=binary-tree
+
+# 257. Binary Tree Paths
+
+# Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+# A leaf is a node with no children.
+
+# Example 1:
+# Input: root = [1,2,3,null,5]
+# Output: ["1->2->5","1->3"]
+
+# Example 2:
+# Input: root = [1]
+# Output: ["1"]
+ 
+# Constraints:
+# The number of nodes in the tree is in the range [1, 100].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional, List
+
+def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+    pass
